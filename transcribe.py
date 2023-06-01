c516d6dfa1567a9bad88328c224cb1753756bc06
Bent Mueller <bentmuller.ai@gmail.com>
2023-06-01 16:30:31 +0000

    Improved user interface a little

--- transcribe.py
+++ transcribe.py
@@ -1,10 +1,12 @@
-from time import time
-import sounddevice as sd
-import soundfile as sf
-import pyperclip
-import whisper
+#! /usr/bin/env python3
 import tempfile
 import warnings
+from time import time
+
+import pyperclip
+import sounddevice as sd
+import soundfile as sf
+import whisper
 
 warnings.filterwarnings("ignore")
 
@@ -29,7 +31,7 @@ class Recorder:
             raise e
 
     def start_recording(self):
-        print("Recording...")
+        print("Recording ", end="")
         self.is_recording = True
         self.start_time = time()
         self.recording = sd.rec(
@@ -43,19 +45,20 @@ class Recorder:
         self.recording = self.recording[: int(elapsed_time * self.fs)]
         sf.write(self.filename, self.recording, self.fs)
 
-        print(f"Recording duration: {elapsed_time:.2f} seconds.")
+        print(f"finished duration: {elapsed_time:.2f} seconds. ", end="")
 
         self.transcribe_audio()
 
     def transcribe_audio(self):
         try:
-            print("Transcribing audio...")
+            print("Transcribing audio now...")
             r = self.model.transcribe(self.filename, temperature=0.0)
-            transcribed_text = r["text"]
-            print("Transcribed: \n\n")
-            print(80 * "-")
+            transcribed_text = str(r["text"]).rstrip().lstrip()
+
+            print("Transcribed: \n")
+            print(80 * "=")
             print(transcribed_text)
-            print(80 * "-")
+            print(80 * "=")
             pyperclip.copy(transcribed_text)
         except Exception as e:
             print("Error during transcription:", str(e))
@@ -74,7 +77,7 @@ class Recorder:
         @bindings.add("q")
         def _(event):
             print("Exiting application.")
-            event.app.exit()
+            raise KeyboardInterrupt
 
         # Create a session with the key bindings
         session = PromptSession(key_bindings=bindings)
@@ -82,15 +85,17 @@ class Recorder:
         while True:
             try:
                 _ = session.prompt("> ")
+
             except KeyboardInterrupt:
                 break
 
 
 if __name__ == "__main__":
     print("Welcome to whisper-transcribe!")
-    print("Instructions:")
+    print("Instructions: \n")
     print("Press 'Space' to start / stop recording.")
-    print("Press 'Q' to quit the application.")
+    print("Press 'Q', Ctrl+D or Ctrl+C to quit the application.")
+    print("Transcriptions are automatically copied to clipboard.")
     print()
 
     with tempfile.NamedTemporaryFile(suffix=".wav") as temp:

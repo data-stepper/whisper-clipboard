2dfe4b65d0f54ed213719cd368d08c88bf7d6e56
Bent Mueller <bentmuller.ai@gmail.com>
2023-06-01 22:59:01 +0000

    Forgot the requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="whisper-clipboard",
     version="0.1.1",
@@ -27,4 +30,5 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
+    install_requires=requirements,
 )
